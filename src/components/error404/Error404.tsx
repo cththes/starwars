f0203f2bad708cf7d9d75a9bcdda4a1a3f0634d5
cth <cththes@gmail.com>
2023-04-05 07:53:12 +0300

    fix error404

--- src/components/error404/Error404.tsx
+++ src/components/error404/Error404.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import styles from "./Error404.module.css";
 import logo from "../../assets/404/Team_Rocket_trio_OS_1.png";
+import background from "../../assets/404/background.png";
+import { useNavigate } from "react-router-dom";
 
 interface Props {}
 
 function Error404(props: Props) {
   const {} = props;
 
+  const onReturnBtnClick = () => {
+    window.history.back();
+  };
+
   return (
     <div className={styles.main}>
-      <div className={styles.container}>
-        <img src={logo} className={styles.error_img} alt=""/>
-        <h1 className={styles.error__h1}>
-          404
-        </h1>
-      </div>
+      <img src={background} className={styles.error__img} alt="" />
+      <button className={styles.error__btn} onClick={onReturnBtnClick}>
+        Return
+      </button>
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import styles from "./Header.module.css"
-import logo from "../../assets/Rectangle48.png"
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/Rectangle48.png";
 import { NavLink } from "react-router-dom";
 
 interface Props {}
 
 function Header(props: Props) {
-   const {} = props
+  const {} = props;
 
-   return (
-      <div className={styles.Header}>
-         <img className={styles.logo} src={logo} alt="logo"/>
-         <NavLink to="/home">Home</NavLink>
-         <NavLink to="/people">Characters</NavLink>
+  return (
+    <div className={styles.Header}>
+      <div className={styles.container}>
+        <div className={styles.logo__container}>
+          <img className={styles.logo} src={logo} alt="logo" />
+        </div>
+        <div>
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/people">Characters</NavLink>
+        </div>
       </div>
-   )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
